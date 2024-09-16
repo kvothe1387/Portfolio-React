85192d4bf58d8ebda2b0b610fa33276ef8b3d729
David McCullough <dmccullough488@gmail.com>
2024-09-16 14:32:51 -0500

    Created contact component

---
+++ React-Portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    < footer >
+      <div>
+        <h2>Contact</h2>
+        <p>Reach out requested please!</p>
+      </div>
+    </footer >
+
+  );
+};
