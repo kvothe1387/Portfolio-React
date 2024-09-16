9902ab0054402251c3c2d91335fe71080694b99b
David McCullough <dmccullough488@gmail.com>
2024-09-16 18:25:03 -0500

    Completed the contact component & styling

--- React-Portfolio/src/components/Contact/Contact.jsx
+++ React-Portfolio/src/components/Contact/Contact.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css"
 
 export const Contact = () => {
   return (
-    < footer >
-      <div>
+    < footer id="contact" className={styles.container}  >
+      <div className={styles.text}>
         <h2>Contact</h2>
         <p>Reach out requested please!</p>
       </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:dmccullough488@gmail.com">dmccullough488@gmail.com</a>
+        </li>
+
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIN icon" />
+          <a href="https://www.linkedin.com/in/david-mccullough-ab2a257a">linkedin.com/in/david-mccullough-ab2a257a</a>
+        </li>
+
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/kvothe1387">github.com/kvothe1387</a>
+        </li>
+
+      </ul>
     </footer >
 
   );
