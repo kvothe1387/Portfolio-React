e2438a12f5d5236d15b7833d3c8b8233dc34b25a
David McCullough <dmccullough488@gmail.com>
2024-11-27 19:55:47 -0600

    refactor: updated app.jsx

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,32 +1,22 @@
 
-import React from "react";
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
-import { Resume } from "./components/Resume/Resume";
-import { Portfolio } from "./components/Portfolio/Portfolio";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
+import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
+import { Navbar } from "./components/Navbar/Navbar";
 import { Contact } from "./components/Contact/Contact";
 import { Hero } from "./components/Hero/Hero";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <div className={styles.App}>
-
-      <Header />
-
-      <Routes>
-        <Route exact path="/" element={<About />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
-      <Footer />
-
-
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </div >
   );
 }
