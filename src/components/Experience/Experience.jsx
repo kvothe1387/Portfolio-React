3b96ec1fe51eb6bae16b4502c97051d39354b72a
David McCullough <dmccullough488@gmail.com>
2024-11-27 21:35:13 -0600

    fix: fixed exp & navbar component import typo

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import styles from "./Resume.module.css";
+import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
-export const Expierence = () => {
+export const Experience = () => {
   return (
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Experience</h2>
